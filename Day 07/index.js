b2f82ee21b789c96867872d892ffb26b34bf7150
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-09-24 23:36:01 +0530

    file update in index

--- Day 07/index.js
+++ Day 07/index.js
@@ -1,37 +1,39 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-var userList = [{
-    username: 'arnab',
-    password: 'password'
-}]
+var userList = [
+	{
+		username: "rishi",
+		password: "password",
+	},
+];
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+	res.render("index");
+});
 
 app.post("/login", (req, res) => {
-    let user = {
-        username : req.query.username,
-        password : req.query.password
-    }
-    userList.push(user)
+	let user = {
+		username: req.query.username,
+		password: req.query.password,
+	};
+	userList.push(user);
 
-    // Do something with this
-    res.send(user)
-})
+	// Do something with this
+	res.send(user);
+});
 
 app.get("/user/:username", (req, res) => {
-    userList.forEach((user) => {
-        if(req.params.username === user.username){
-            res.send(userList)
-        }
-    })
-    res.send("Username not found")
-})
+	userList.forEach((user) => {
+		if (req.params.username === user.username) {
+			res.send(userList);
+		}
+	});
+	res.send("Username not found");
+});
 
-app.listen(3000, () => console.log("Listing at http://localhost:3000"))
+app.listen(3000, () => console.log("Listing at http://localhost:3000"));
